6fff63012387bdda181f0d2e61d37d732fb1a7b9
Rudresh Sharma <75907270+rudy007225@users.noreply.github.com>
2024-07-05 05:20:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ if __name__ == '__main__':
 text = ("WELCOME TO ROBO-SPEAKER 1.0 created by RUDY "
     print(f"---------{text}---------\n ")
 
-    while(True):
+speak = wincom.Dispatch("SAPI.spVoice")
+speak.Speak(text)
 
-        speak = wincom.Dispatch("SAPI.spVoice")
-        speak.Speak(text)
+    while(True):
 
         read = input("Enter text-to-speech info here : ")
         if text == 'turn off speaker':
             break
+
         speak.Speak(read)
         time.sleep(3)
